2cb564a91129790b63f9180d56fc9e5eae9a2eb4
saahir <33964080+saahirlol@users.noreply.github.com>
2023-11-18 14:37:15 -0600

    Update start-tailscale.sh

--- start-tailscale.sh
+++ start-tailscale.sh
@@ -7,9 +7,9 @@ sleep 5
 
 # Check if LOGIN_KEY is provided and use it if it is
 if [ -n "$LOGIN_KEY" ]; then
-    tailscale up --login-server=$HEADSCALE_SERVER_URL --authkey=$LOGIN_KEY
+    tailscale up --login-server=$HEADSCALE_SERVER_URL --authkey=$LOGIN_KEY --hostname=$NOMBRE
 else
-    tailscale up --login-server=$HEADSCALE_SERVER_URL
+    tailscale up --login-server=$HEADSCALE_SERVER_URL --hostname=$NOMBRE
 fi
 
 # Keep the container running
